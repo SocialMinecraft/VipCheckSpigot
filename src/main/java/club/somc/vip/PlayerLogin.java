6b138b86feca0b6aa5573dfa78e976924fef6574
Dylan Garvis <dylan@garvis.dev>
2024-04-01 20:48:06 -0400

    added checks for memberships.

--- src/main/java/club/somc/vip/PlayerLogin.java
+++ src/main/java/club/somc/vip/PlayerLogin.java
@@ -1,5 +1,8 @@
 package club.somc.vip;
 
+import club.somc.protos.vip.GetRequest;
+import club.somc.protos.vip.GetResponse;
+import com.google.protobuf.InvalidProtocolBufferException;
 import io.nats.client.Connection;
 import io.nats.client.Message;
 import net.luckperms.api.LuckPerms;
@@ -11,6 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.time.Duration;
+import java.util.Date;
 
 public class PlayerLogin implements Listener {
 
@@ -27,16 +31,48 @@ public class PlayerLogin implements Listener {
         // Send a request to check the player's vip status.
 
         //event.getPlayer().hasPermission("group.vip");
-        //User user = lp.getPlayerAdapter(Player.class).getUser(event.getPlayer());
+        User user = lp.getPlayerAdapter(Player.class).getUser(event.getPlayer());
+        Bukkit.getLogger().info(event.getPlayer().getName() + " primary group is " + user.getPrimaryGroup());
         //user.setPrimaryGroup("vip/default");
 
 
+        GetRequest req = GetRequest.newBuilder()
+                .setMinecraftUuid(event.getPlayer().getUniqueId().toString())
+                .build();
+
+
+        Message m;
         try {
-            Message m = nc.request("vip.status", null, Duration.ofMillis(300));
+            m = nc.request("vip.get", req.toByteArray(), Duration.ofMillis(300));
         } catch (InterruptedException e) {
             Bukkit.getLogger().warning(e.getMessage());
             return;
         }
+
+        GetResponse res;
+        try {
+             res = GetResponse.parseFrom(m.getData());
+        } catch (InvalidProtocolBufferException e) {
+            Bukkit.getLogger().warning(e.getMessage());
+            return;
+        }
+
+        // do they have a membership that is not expired?
+        if (res.hasMembership() && new Date(res.getMembership().getExpire()).before(new Date())) {
+            // set their vip to true.
+            if (!user.getPrimaryGroup().equals("vip")) {
+                user.setPrimaryGroup("vip");
+                Bukkit.getLogger().info(event.getPlayer().getName() + " primary group changed to " + user.getPrimaryGroup());
+            }
+            return;
+        }
+
+        // Set their vip to false
+        if (!user.getPrimaryGroup().equals("default")) {
+            user.setPrimaryGroup("default");
+            Bukkit.getLogger().info(event.getPlayer().getName() + " primary group changed to " + user.getPrimaryGroup());
+        }
+
 
     }
 }
